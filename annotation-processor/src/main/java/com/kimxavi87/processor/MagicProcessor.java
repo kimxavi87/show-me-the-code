d27e36fa1bee6e5bf31a8ea6d9ec16033c740df9
kimxavi87 <kimxavi87@naver.com>
2021-10-11 19:26:09 +0900

    annotation processor 소스 generated 하도록 구현

--- annotation-processor/src/main/java/com/kimxavi87/processor/MagicProcessor.java
+++ annotation-processor/src/main/java/com/kimxavi87/processor/MagicProcessor.java
@@ -1,15 +1,22 @@
 package com.kimxavi87.processor;
 
 import com.google.auto.service.AutoService;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.Name;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -31,6 +38,30 @@ public class MagicProcessor extends AbstractProcessor {
                 processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "process " + simpleName);
             } else {
                 processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "can not process " + simpleName);
+            }
+
+            TypeElement typeElement = (TypeElement) element;
+            ClassName className = ClassName.get(typeElement);
+
+            MethodSpec methodSpec = MethodSpec.methodBuilder("")
+                    .addModifiers(Modifier.PUBLIC)
+                    .returns(String.class)
+                    .addStatement("return $S", "Rabbit!")
+                    .build();
+
+            TypeSpec typeSpec = TypeSpec.classBuilder("MagicMoja")
+                    .addModifiers(Modifier.PUBLIC)
+                    .addMethod(methodSpec)
+                    .addSuperinterface(className)
+                    .build();
+
+            Filer filer = processingEnv.getFiler();
+            try {
+                JavaFile.builder(className.packageName(), typeSpec)
+                        .build()
+                        .writeTo(filer);
+            } catch (IOException e) {
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "can not create file " + simpleName);
             }
         }
 
