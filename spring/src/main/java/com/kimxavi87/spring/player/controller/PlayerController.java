651c067c8b0392673a9d786690bc802fde189a19
kimxavi87 <kimxavi87@naver.com>
2021-10-13 02:35:10 +0900

    Controller 에 파라미터 PageableDefault 적용 + 적용하려면 WebConfig 추가 필요

---
+++ spring/src/main/java/com/kimxavi87/spring/player/controller/PlayerController.java
@@ -0,0 +1,23 @@
+package com.kimxavi87.spring.player.controller;
+
+import com.kimxavi87.spring.player.entity.Member;
+import com.kimxavi87.spring.player.reposiotry.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class PlayerController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members")
+    public List<Member> requestMembers(@PageableDefault(size = 10) Pageable pageable) {
+        return memberRepository.findAll(pageable);
+    }
+}
