b343dd0d5738230650eed85f82658601d137e3d3
kimxavi87 <kimxavi87@naver.com>
2021-10-21 16:13:38 +0900

    설명 추가 수정

--- spring/src/test/java/com/kimxavi87/spring/palyer/TeamControllerTests.java
+++ spring/src/test/java/com/kimxavi87/spring/palyer/TeamControllerTests.java
@@ -18,7 +18,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-// 통합테스트할 때는 아래와 같이 @MockMvcTest 는 Controller만 테스트해줄 때 사용 (내부 bean들이 mocking 됨)
+// 통합테스트할 때는 아래와 같이 @SpringBootTest, @AutoConfigureMockMvc
+// @MockMvcTest 는 Controller만 테스트해줄 때 사용 (내부 bean들이 mocking 됨)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class TeamControllerTests {
