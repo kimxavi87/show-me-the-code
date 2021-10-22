fc99f7b70c265bc4db382e54cf79ec79fb354ab3
kimxavi87 <kimxavi87@naver.com>
2021-10-22 12:56:21 +0900

    #16 sendTransactionally 에 대한 설정으로 되어야 성공하는 설정들이어서 일반적인 send 로 변경
    send, receive 간단한 테스트 코드 추가

--- reactive-streams/src/test/java/com/kimxavi87/reactivestreams/EmbeddedKafkaTests.java
+++ reactive-streams/src/test/java/com/kimxavi87/reactivestreams/EmbeddedKafkaTests.java
@@ -2,20 +2,22 @@ package com.kimxavi87.reactivestreams;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
 import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
 import org.springframework.kafka.support.converter.MessagingMessageConverter;
 import org.springframework.kafka.test.condition.EmbeddedKafkaCondition;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
-import org.springframework.test.annotation.DirtiesContext;
+import reactor.core.publisher.Mono;
 import reactor.kafka.receiver.ReceiverOptions;
 import reactor.kafka.sender.SenderOptions;
+import reactor.kafka.sender.SenderResult;
+import reactor.test.StepVerifier;
 
 import java.time.Duration;
 import java.util.Collections;
@@ -30,6 +32,8 @@ public class EmbeddedKafkaTests {
     private static final int DEFAULT_PARTITION = 1;
     private static final String CONSUMER_GROUP_ID = "test-consumer-group";
     private static final Duration DEFAULT_VERIFY_TIMEOUT = Duration.ofSeconds(10);
+    private static final String DEFAULT_VALUE = "test-value";
+
     private static ReactiveKafkaConsumerTemplate<Integer, String> kafkaConsumer;
     private ReactiveKafkaProducerTemplate<Integer, String> kafkaProducer;
 
@@ -50,11 +54,7 @@ public class EmbeddedKafkaTests {
     private SenderOptions<Integer, String> setupSenderOptionsWithDefaultTopic() {
         Map<String, Object> senderProps =
                 KafkaTestUtils.producerProps(EmbeddedKafkaCondition.getBroker().getBrokersAsString());
-        SenderOptions<Integer, String> senderOptions = SenderOptions.create(senderProps);
-        senderOptions = senderOptions
-                .producerProperty(ProducerConfig.TRANSACTIONAL_ID_CONFIG, "reactive.transaction")
-                .producerProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
-        return senderOptions;
+        return SenderOptions.create(senderProps);
     }
 
     private static ReceiverOptions<Integer, String> setupReceiverOptionsWithDefaultTopic(
@@ -63,8 +63,6 @@ public class EmbeddedKafkaTests {
         ReceiverOptions<Integer, String> basicReceiverOptions = ReceiverOptions.create(consumerProps);
         return basicReceiverOptions
                 .consumerProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
-                .consumerProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false")
-                .consumerProperty(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed")
                 .addAssignListener(p -> assertThat(p.iterator().next().topicPartition().topic())
                         .isEqualTo(TOPIC))
                 .subscription(Collections.singletonList(TOPIC));
@@ -77,7 +75,25 @@ public class EmbeddedKafkaTests {
 
     @Test
     public void test() {
-        System.out.println("test hello");
+        Mono<SenderResult<Void>> senderResultMono =
+                this.kafkaProducer.send(TOPIC, DEFAULT_VALUE);
 
+        StepVerifier.create(senderResultMono)
+                .assertNext(senderResult -> {
+                    assertThat(senderResult.recordMetadata())
+                            .extracting(RecordMetadata::topic)
+                            .isEqualTo(TOPIC);
+                })
+                .expectComplete()
+                .verify(DEFAULT_VERIFY_TIMEOUT);
+
+        StepVerifier
+                .create(kafkaConsumer
+                        .receive()
+                        .doOnNext(rr -> System.out.println(rr.value()))
+                        .doOnNext(rr -> rr.receiverOffset().acknowledge()))
+                .assertNext(receiverRecord -> assertThat(receiverRecord.value()).isEqualTo(DEFAULT_VALUE))
+                .thenCancel()
+                .verify(DEFAULT_VERIFY_TIMEOUT);
     }
 }
