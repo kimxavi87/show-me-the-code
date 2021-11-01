9d9d1df24dca78e64e7d871b5c5d2159c0b39820
kimxavi87 <kimxavi87@naver.com>
2021-11-01 17:58:59 +0900

    Mongo Repository 비교 필요함

--- reactive-streams/src/main/java/com/kimxavi87/reactivestreams/Customer/CustomerReactiveRepository.java
+++ reactive-streams/src/main/java/com/kimxavi87/reactivestreams/Customer/CustomerReactiveRepository.java
@@ -2,5 +2,6 @@ package com.kimxavi87.reactivestreams.Customer;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
+// ReactiveCrudRepository 는 ReactiveMongoRepository 부모로 존재
 public interface CustomerReactiveRepository extends ReactiveMongoRepository<Customer, String> {
 }
