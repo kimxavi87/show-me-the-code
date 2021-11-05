c3dbf9c36a8c95b1ac202480cf0e204d16f741e0
kimxavi87 <kimxavi87@naver.com>
2021-11-05 20:52:35 +0900

    MongoDB ReactiveMongoRepository도 테스트 해봄

--- reactive-streams/src/main/java/com/kimxavi87/reactivestreams/Customer/CustomerReactiveRepository.java
+++ reactive-streams/src/main/java/com/kimxavi87/reactivestreams/Customer/CustomerReactiveRepository.java
@@ -1,7 +1,9 @@
 package com.kimxavi87.reactivestreams.Customer;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 // ReactiveCrudRepository 는 ReactiveMongoRepository 부모로 존재
 public interface CustomerReactiveRepository extends ReactiveMongoRepository<Customer, String> {
+    Mono<Customer> findByName(String name);
 }
--- reactive-streams/src/test/java/com/kimxavi87/reactivestreams/mongodb/CustomerRepositoryTests.java
+++ reactive-streams/src/test/java/com/kimxavi87/reactivestreams/mongodb/CustomerRepositoryTests.java
@@ -1,7 +1,8 @@
-package com.kimxavi87.reactivestreams;
+package com.kimxavi87.reactivestreams.mongodb;
 
 import com.kimxavi87.reactivestreams.Customer.Customer;
 import com.kimxavi87.reactivestreams.Customer.CustomerCrudRepository;
+import com.kimxavi87.reactivestreams.Customer.CustomerReactiveRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,11 +16,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CustomerRepositoryTests {
     @Autowired
     private CustomerCrudRepository crudRepository;
+    @Autowired
+    private CustomerReactiveRepository reactiveRepository;
 
     @Test
     public void givenCustomer_whenSaveAndFindByName_thenSuccess() {
         Customer park = crudRepository.save(new Customer(null, "park", 19990909)).block();
         StepVerifier.create(crudRepository.findByName("park"))
+                .assertNext(customer -> {
+                    System.out.println(customer.getId());
+                    assertThat(customer).isEqualTo(park);
+                })
+                .expectComplete()
+                .verify(Duration.ofSeconds(30));
+    }
+
+    @Test
+    public void givenCustomer_whenReactiveSaveAndFindByName_thenSuccess() {
+        Customer park = reactiveRepository.save(new Customer(null, "park", 19990909)).block();
+        StepVerifier.create(reactiveRepository.findByName("park"))
                 .assertNext(customer -> {
                     System.out.println(customer.getId());
                     assertThat(customer).isEqualTo(park);
