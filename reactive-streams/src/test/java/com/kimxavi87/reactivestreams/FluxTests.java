b40d703344940d0cd9d3297162571506676d15de
kimxavi87 <kimxavi87@naver.com>
2021-11-10 22:47:38 +0900

    note,,

--- reactive-streams/src/test/java/com/kimxavi87/reactivestreams/FluxTests.java
+++ reactive-streams/src/test/java/com/kimxavi87/reactivestreams/FluxTests.java
@@ -19,6 +19,14 @@ public class FluxTests {
         log.info("END");
     }
 
+    // Fuseable 있고 없고 차이는 Subscription이 QueueSubscription를 interface 상속 하느냐 마느냐 차이
+    // QueueSubscription을 내부 필드로 가지고, sourceMode 를 또 가짐
+    // => Queue 는 poll/clear/size/isEmpty 만 internal로 사용한다
+    // onNext 다름 - sourceMode 가 ASYNC 이면 onNext(null) 보냄
+    // requestFusion
+
+    // conditional vs normal
+
     @Test
     public void interval() throws InterruptedException {
         Flux.interval(Duration.ofSeconds(1))
