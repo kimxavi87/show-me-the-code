ccc2ae0d374a85401dc9f7f7aded627db2228fbd
kimxavi87 <kimxavi87@naver.com>
2022-10-27 14:08:48 +0900

    kafka-reactor 테스트 추가

--- reactive-streams/src/test/java/com/kimxavi87/reactivestreams/EmbeddedKafkaTests.java
+++ reactive-streams/src/test/java/com/kimxavi87/reactivestreams/EmbeddedKafkaTests.java
@@ -1,5 +1,6 @@
 package com.kimxavi87.reactivestreams;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import org.junit.jupiter.api.AfterEach;
@@ -16,6 +17,7 @@ import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import reactor.kafka.receiver.ReceiverOptions;
 import reactor.kafka.sender.SenderOptions;
 import reactor.kafka.sender.SenderResult;
@@ -27,6 +29,7 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 @EmbeddedKafka(topics = EmbeddedKafkaTests.TOPIC,
         brokerProperties = { "transaction.state.log.replication.factor=1", "transaction.state.log.min.isr=1" })
 public class EmbeddedKafkaTests {
@@ -44,6 +47,8 @@ public class EmbeddedKafkaTests {
 
         Map<String, Object> consumerProps =
                 KafkaTestUtils.consumerProps(CONSUMER_GROUP_ID, "false", broker);
+        consumerProps.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, 5);
+
         kafkaConsumer =
                 new ReactiveKafkaConsumerTemplate<>(setupReceiverOptionsWithDefaultTopic(consumerProps));
 
@@ -107,25 +112,36 @@ public class EmbeddedKafkaTests {
     public void givenStringValue_whenHappenException_thenKeepReceive() throws InterruptedException {
         Flux.interval(Duration.ofSeconds(1))
                 .flatMap(l -> this.kafkaProducer.send(TOPIC, DEFAULT_VALUE))
-                .doOnNext(voidSenderResult -> System.out.println(voidSenderResult.recordMetadata().topic()))
+//                .doOnNext(voidSenderResult -> System.out.println(voidSenderResult.recordMetadata().topic()))
                 .subscribe();
+        // commit 하지 않으면 poll 되지 않음
+        // 다른 쓰레드로 작업을 계속 진행해도 poll, commit 을 하지 않음
 
         // map 안에서 exception 던지면
         // cancel 이나 complete 가 발생하지 않고
         // error 발생
         // consume도 더 하지 않는다 (문제 상황 확인 완료)
         // catch 하면 계속 진행됨
-        kafkaConsumer.receive()
-                .doOnNext(consumerRecord -> System.out.println("[CONSUME]" + consumerRecord.toString()))
+        kafkaConsumer.receiveAutoAck()
+//                .publishOn(Schedulers.newSingle("hello-world-"))
+                .doOnNext(consumerRecord -> log.info("[CONSUME] {}", consumerRecord.toString()))
                 .map(rr -> {
+                    try {
+                        Thread.sleep(1000 * 10);
+                        log.info("[CONSUME] AFTER SLEEP");
+                    } catch (InterruptedException e) {
+                        return "";
+                    }
+                    log.info("[CONSUME] PROCESS ");
+                    return "";
 //                    try {
-                        throw new RuntimeException("Hello World!");
+//                        throw new RuntimeException("Hello World!");
 //                    } finally {
 //                        System.out.println("Hello Finally");
 //                        rr.receiverOffset().acknowledge();
 //                    }
                 })
-                .onErrorContinue((e, o) -> System.out.println("ERORR CON"))
+                .onErrorContinue((e, o) -> log.error("error", e))
                 .doOnNext(s -> System.out.println("MAPMAP"))
                 .subscribe();
 //        kafkaConsumer.receiveAutoAck()
@@ -145,6 +161,6 @@ public class EmbeddedKafkaTests {
 //                .doOnComplete(() -> System.out.println("complete"))
 //                .subscribe();
 
-        Thread.sleep(30 * 1000);
+        Thread.sleep(5000 * 1000);
     }
 }
