ee88dc9f13cfbdeee9ae404dd1a87f8b830b4a78
kimxavi87 <kimxavi87@naver.com>
2023-02-13 18:56:25 +0900

    #107 reactor OnError 들에 대해서 테스트 해봄

--- reactive-streams/src/test/java/com/kimxavi87/reactivestreams/reactor/ErrorTests.java
+++ reactive-streams/src/test/java/com/kimxavi87/reactivestreams/reactor/ErrorTests.java
@@ -46,6 +46,24 @@ public class ErrorTests {
     }
 
     @Test
+    public void onErrorContinueWith() {
+        Flux.range(0, 10)
+                .map(i -> i * 2)
+                .doOnNext(integer -> log.info("before {}", integer))
+                .map(i -> i / 0)
+                .doOnNext(integer -> log.info("before-e {}", integer))
+                .doOnError(integer -> log.info("before-e"))
+                .log()
+                .onErrorContinue((throwable, i) -> {
+                    log.error("error", throwable);
+                })
+                .onErrorReturn(1000)
+                .doOnNext(integer -> log.info("after-n {}", integer))
+                .doOnError(integer -> log.info("after-e"))
+                .subscribe();
+    }
+
+    @Test
     public void onErrorResume() {
         // onErrorResume -> 스트림 끝남
         Flux.range(0, 10)
@@ -57,6 +75,23 @@ public class ErrorTests {
                     //return Mono.empty();
                     return Mono.just(10);
                 })
+                .subscribe();
+    }
+
+    @Test
+    public void onErrorResumeWithOnErrorContinue() {
+        // onErrorResume -> 스트림이 error로 계속 전달됨
+        // onErrorContinue -> continue; 문 처럼 loop 의 위로 올라가는 것
+        Flux.range(0, 10)
+                .map(i -> i * 2)
+                .doOnNext(integer -> log.info("{}", integer))
+                .map(i -> i / 0)
+                .onErrorResume(throwable -> {
+                    log.error("error", throwable);
+                    //return Mono.empty();
+                    return Mono.just(10);
+                })
+                .onErrorContinue((throwable, o) -> log.info("continue {}", o))
                 .subscribe();
     }
 }
