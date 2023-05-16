4efa59e9be13430242c41c92e4588079d49a835a
kimxavi87 <kimxavi87@naver.com>
2023-05-16 16:09:54 +0900

    #108 리눅스일 때만 테스트 작동하도록 추가

--- normal/src/test/java/com/kimxavi87/normal/process/ProcessUtilsTests.java
+++ normal/src/test/java/com/kimxavi87/normal/process/ProcessUtilsTests.java
@@ -3,10 +3,13 @@ package com.kimxavi87.normal.process;
 import com.kimxavi87.normal.linux.LinuxCommands;
 import com.kimxavi87.normal.linux.LinuxCommandsImpl;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 public class ProcessUtilsTests {
 
     @Test
+    @EnabledOnOs(OS.LINUX)
     public void test() {
         LinuxCommands linuxCommands = new LinuxCommandsImpl();
         linuxCommands.ps();
