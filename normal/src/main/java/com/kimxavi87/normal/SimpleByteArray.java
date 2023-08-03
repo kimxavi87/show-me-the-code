131c63f3d995315757fd39acbdf36c31f557197d
kimxavi87 <kimxavi87@naver.com>
2023-08-03 18:50:44 +0900

    simplebytearray #113

--- normal/src/main/java/com/kimxavi87/normal/SimpleByteArray.java
+++ normal/src/main/java/com/kimxavi87/normal/SimpleByteArray.java
@@ -1,8 +1,12 @@
 package com.kimxavi87.normal;
 
 public class SimpleByteArray {
-    private final byte[] byteArr;
+    private byte[] byteArr;
     private int idx;
+
+    public SimpleByteArray() {
+        this(16);
+    }
 
     public SimpleByteArray(int size) {
         this.idx = 0;
@@ -11,10 +15,20 @@ public class SimpleByteArray {
 
     public void add(byte b) {
         if (idx == byteArr.length) {
-            throw new RuntimeException("Array is full");
+            resize(byteArr.length * 2);
         }
         byteArr[idx] = b;
         idx++;
+    }
+
+    public void resize(int size) {
+        if (size() > size) {
+            throw new IllegalArgumentException();
+        }
+
+        byte[] newArr = new byte[size];
+        System.arraycopy(byteArr, 0, newArr, 0, idx);
+        byteArr = newArr;
     }
 
     public void add(byte[] byteArr) {
@@ -31,5 +45,9 @@ public class SimpleByteArray {
 
     public int size() {
         return idx;
+    }
+
+    public int length() {
+        return byteArr.length;
     }
 }
---
+++ normal/src/test/java/com/kimxavi87/normal/dns/SimpleByteArrayTests.java
@@ -0,0 +1,28 @@
+package com.kimxavi87.normal.dns;
+
+import com.kimxavi87.normal.SimpleByteArray;
+import org.junit.jupiter.api.Test;
+
+public class SimpleByteArrayTests {
+
+    @Test
+    void add() {
+        SimpleByteArray simpleByteArray = new SimpleByteArray(3);
+        simpleByteArray.add((byte) 1);
+        simpleByteArray.add((byte) 2);
+        simpleByteArray.add((byte) 3);
+
+        assert simpleByteArray.length() == 3;
+
+        simpleByteArray.add((byte) 4);
+
+        assert simpleByteArray.length() != 3;
+
+        byte[] byteArr = simpleByteArray.getByteArr();
+
+        assert byteArr[0] == (byte) 1;
+        assert byteArr[1] == (byte) 2;
+        assert byteArr[2] == (byte) 3;
+        assert byteArr[3] == (byte) 4;
+    }
+}
